e8d03619d0baa9de44c15242bcf3884caeff1f68
contreras48 <contreras4893@gmail.com>
2024-10-09 17:34:56 -0600

    part2: courseinfo, step eight finished

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,6 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <p><strong>total  of {sum} exercises</strong></p>
 
 const Part = ({ part }) =>
   <p>
@@ -17,11 +17,13 @@ const Content = ({ parts }) =>
   </>
 
 const Course = ({ course }) => {
+  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
+
   return (
     <>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total sum={course.parts.reduce((s, p) => s + p.exercises, 0)} />
+      <Total sum={total} />
     </>
   )
 }
