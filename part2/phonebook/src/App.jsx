dff601970f3489fbe9f1dc39277091d0e72e09ae
contreras48 <contreras4893@gmail.com>
2024-10-11 18:37:30 -0600

    part2: phonebook, step six finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,15 @@
+import { useState, useEffect } from 'react'
+import axios  from 'axios';
+
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Phonebook from './components/PhoneBook'
 
-import { useState } from 'react'
-
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('');
+  const [persons, setPersons] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -32,6 +28,14 @@ const App = () => {
   const showContacts = filter === '' 
     ? persons
     : persons.filter(p => p.name.toLowerCase().includes(filter))
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, [])
   
   return (
     <div>
