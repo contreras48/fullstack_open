99c909b8008befade5ae99bf66c9eb0a23b02b5f
contreras48 <contreras4893@gmail.com>
2025-04-27 20:08:48 -0600

    part3: phonebook_backend, step thirteen and fourteen

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
+const Person =  require('./models/person');
 
 const app = express();
-
-
 
 morgan.token('body', (req, res) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : "";
@@ -15,33 +15,10 @@ app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json());
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-const newId = () => Math.floor(Math.random() * 10000);
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 });
 
 app.get('/info', (request, response) => {
@@ -55,18 +32,10 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const person = persons.find(person => person.id === id);
-
-  if(person){
-    response.json(person);
-  }else{
-    response.status(404).json({
-      error: 'person not found'
-    });
-  }
-  
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -93,21 +62,15 @@ app.post('/api/persons', (request, response) => {
     });
   }
 
-  const person = {
-    id: newId(),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  });
 
-  if(persons.some(p => p.name === person.name)){
-    return response.status(400).json({
-      error: 'name must be unique'
-    });
-  }
-
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then( savedPerson => {
+    console.log(`added ${savedPerson.name} number ${savedPerson.number} to phonebook`);
+     response.json(savedPerson);
+  })
 });
 
 
