a48713ebaa44b994dfc4c5366e6d44a294e59d17
contreras48 <contreras4893@gmail.com>
2025-05-12 19:51:50 -0600

    part3: phonebook_backend, exercice 3.20 and 3.21

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -23,7 +23,13 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     requiered: true,
-    minlength: 8
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number`
+    }
   },
 })
 
